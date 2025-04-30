f3439b8d1d95aa1359cc8b6bc838f9f2d7f9b651
Ojota-c <joaocb202@gmail.com>
2025-04-30 13:42:06 -0300

    update2

---
+++ aula11/index.js
@@ -0,0 +1,13 @@
+/*
+*Aritméticos
+* + Adição / Concatenação
+* - Subtração
+* * Multiplicação
+* / Divisão
+* % Resto da Divisão
+* ** Potenciação
+*/
+const num1 = 10;
+const num2 = "5";
+
+console.log(num1 + num2); 
