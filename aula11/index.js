89bf2c735ad8a88c4044adf982e951399ae82e21
Ojota-c <joaocb202@gmail.com>
2025-04-30 16:45:43 -0300

    update2

--- aula11/index.js
+++ aula11/index.js
@@ -6,8 +6,10 @@
 * / Divisão
 * % Resto da Divisão
 * ** Potenciação
+NaN - Not a Number
 */
 const num1 = 10;
-const num2 = "5";
+const num2 = Number("5");
 
-console.log(num1 + num2); 
+console.log(num1 * num2); 
+console.log(typeof num2);
---
+++ aula12/js/script.js
@@ -0,0 +1,29 @@
+let num1 = prompt("Digite o primeiro número");
+let sinal = prompt("Digite o sinal da operação desejada (+, -, *, /)");
+let num2 = prompt("Digite o segundo número");
+
+
+num1 = parseInt(num1);
+num2 = Number(num2);
+
+let resultado
+
+if (sinal === "+") {
+    resultado = num1 + num2;
+} else if (sinal === "-") {
+    resultado = num1 - num2;
+} else if (sinal === "*") {
+    resultado = num1 * num2;
+} else if (sinal === "/") {
+    if (num2 !== 0) { // Verifica divisão por zero
+        resultado = num1 / num2;
+    } else {
+        alert("Erro: Divisão por zero não é permitida.");
+        resultado = "Indefinido";
+    }
+} else {
+    alert("Erro: Sinal inválido.");
+    resultado = "Indefinido";
+}
+
+alert(`O resultado da operção de ${num1} ${sinal} ${num2} é: ${resultado}`);	
