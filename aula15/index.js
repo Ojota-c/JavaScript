b66012d7c76058fa55618d91594c8f4d9265a645
Ojota-c <joaocb202@gmail.com>
2025-05-05 14:03:02 -0300

    update2

--- aula15/index.js
+++ aula15/index.js
@@ -1,10 +1,11 @@
+//let num2 = Math.floor(num1); Arredonda para baixo
+//let num2 = Math.ceil(num1); Arredonda para cima
+//let num2 = Math.round(num1); Arredonda para o inteiro mais próximo
+//let num2 = Math.max(1, 2, 3, 4, 5); Retorna o maior número
+//let num2 = Math.min(1, 2, 3, 4, 5); Retorna o menor número
+//let num2 = Math.random(); Retorna um número aleatório entre 0 e 1
+//const num2 = Math.random() * (10 - 5) + 5;  Retorna um número aleatório entre 5 e 10
+//let aleatorioArredondado = Math.round(num2);  Arredonda o número aleatório
+//let num2 = Math.PI;  Retorna o valor de PI
 let num1 = 9.54578;
-//let num2 = Math.floor(num1); // Arredonda para baixo
-//let num2 = Math.ceil(num1); // Arredonda para cima
-//let num2 = Math.round(num1); // Arredonda para o inteiro mais próximo
-//let num2 = Math.max(1, 2, 3, 4, 5); // Retorna o maior número
-//let num2 = Math.min(1, 2, 3, 4, 5); // Retorna o menor número
-//let num2 = Math.random(); // Retorna um número aleatório entre 0 e 1
-const num2 = Math.random() * (10 - 5) + 5; // Retorna um número aleatório entre 5 e 10
-let aleatorioArredondado = Math.round(num2); // Arredonda o número aleatório
 console.log(aleatorioArredondado); 
