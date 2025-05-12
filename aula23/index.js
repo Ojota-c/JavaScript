34861d3294a11daf219ff15e819e734a39719554
Ojota-c <joaocb202@gmail.com>
2025-05-12 18:21:01 -0300

    update

---
+++ aula23/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores Lógicos
+&& --> AND --> E --> Todas as expressões precisam ser verdadeiras para poder retornar True
+|| --> OR --> OU
+! --> NOT --> NÃO
+*/
+//const expressaoAnd = true && true && true && true
+//const expressaoOr = true || false || false || false
+const usuario = 'João';
+const senha = '123456';
+
+const login = usuario === 'João' && senha === '123456'; 
+console.log(login);
